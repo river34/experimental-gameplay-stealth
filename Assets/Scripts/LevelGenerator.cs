59a1df9a3aa2373890d864c3656316f490f68666
river34 <river.lyuxin@gmail.com>
2017-03-04 13:48:21 -0800

    Add more levels.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -192,10 +192,12 @@ public class LevelLibrary {
 	public LevelLibrary ()
 	{
 		levels = new List<Level> ();
-		levels.Add (new Level (0, 0, 0, 4, new int[] {0, 0, 1, 2, 0}));
-		levels.Add (new Level (0, 1, 4, 6, new int[] {0, 1, 2, 0, 1, 1, 2, 2}));
-		levels.Add (new Level (0, 1, 100, 8, new int[] {1, 2, 3, 2, 1, 2, 3, 2}));
-		levels.Add (new Level (0, 2, 100, 10, new int[] {1, 3, 2, 2, 3, 3, 2, 3, 1}));
+		levels.Add (new Level (0, 0, 0, 4, new int[] {0, 1, 1, 2, 1, 1, 0}));
+		levels.Add (new Level (0, 1, 4, 6, new int[] {0, 1, 2, 1, 2, 1, 2, 1, 0}));
+		levels.Add (new Level (0, 1, 10, 8, new int[] {1, 2, 3, 2, 1, 2, 3, 2, 1}));
+		levels.Add (new Level (0, 2, 100, 10, new int[] {1, 3, 2, 2, 3, 3, 2, 2, 3, 1}));
+		levels.Add (new Level (0, 3, 100, 12, new int[] {1, 3, 2, 3, 4, 3, 2, 3, 4, 3, 2, 3, 1}));
+		levels.Add (new Level (0, 3, 100, 14, new int[] {1, 2, 3, 1, 4, 2, 3, 2, 4, 1, 3, 2, 1}));
 	}
 
 	public Level GetLevel (int id)
